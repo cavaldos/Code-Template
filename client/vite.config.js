08d354aa7b97b0e1cc11d26e8077d7f203f09605
Cavaldos <khanhnguyen297kn@gmail.com>
2025-02-04 23:35:27 +0700

    init

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(),],
+  server: {
+    host: "0.0.0.0", // default: 'localhost'
+    hmr: {
+      overlay: false,
+    },
+  },
+  preview: {
+    port: 5175,
+  },
+  resolve: {
+    alias: [
+      {
+        find: "~/",
+        replacement: "/src/",
+      },
+    ],
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 600
+  },
+});
